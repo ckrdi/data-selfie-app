4f9ae618f0682ffcc54b4228dda0a3393b672977
ckrdi <agung.cokorda@gmail.com>
2021-03-20 14:40:03 +0800

    POST request, parse, response, and button

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
   console.log('listening at http://localhost:' + port)
 });
-
+// serving static page
 app.use(express.static('public'));
+
+// json parser
+app.use(express.json());
+
+const allData = [];
+
+// POST handler
+app.post('/api', (request, response) => {
+  const data = request.body
+  allData.push(data);
+  console.log(allData);
+  fs.writeFileSync('data.json', JSON.stringify(allData));
+  response.send(allData);
+});
--- public/script.js
+++ public/script.js
@@ -15,15 +15,37 @@ const marker = L.marker([0, 0]).addTo(mymap);
 // getting the current position and then display it on the webpage
 if ('geolocation' in navigator) {
   console.log('geolocation available');
-  navigator.geolocation.getCurrentPosition((position) => {
-    
-    document.getElementById('lat').textContent = position.coords.latitude;
-    document.getElementById('lon').textContent = position.coords.longitude;
 
-    // setting the map according to the geolocation
-    mymap.setView([position.coords.latitude, position.coords.longitude], 15);
-    marker.setLatLng([position.coords.latitude, position.coords.longitude]);
-  });
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      document.getElementById('lat').textContent = lat;
+      document.getElementById('lon').textContent = lon;
+
+      // setting the map according to the geolocation
+      mymap.setView([lat, lon], 15);
+      marker.setLatLng([lat, lon]);
+
+      const response = await fetch('/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+          lat,
+          lon
+        })
+      });
+      const data = await response.json();
+      console.log(data);
+    })
+  }
+  getLocation();
 } else {
   console.log('geolocation not available');
-}
+};
+
+document.getElementById('postRequest').addEventListener('click', getLocation);
